322d8cb47cd001d47b5ad19464847fa9f14d5104
mishizaki <love.ms.is12t@hotmail.co.jp>
2024-02-13 22:55:25 +0900

    initial.

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿const string menuData = """
+{
+    "101": { "濃厚 MAX ラーメン": 1080 },
+    "102": { "MAX ラーメン": 980 },
+    "103": { "ラーメン": 680 },
+    "201": { "叉焼スペシャル炒飯": 980 },
+    "202": { "叉焼炒飯": 680 }
+}
+""";
+
+var menu = System.Text.Json.JsonSerializer.Deserialize<IDictionary<string, IDictionary<string, int>>>(menuData)!;
+
+var kingaku = Enumerable.Range(0, args.Length / 2).Select(i => i * 2 + 1).Aggregate(0, (sum, i) => menu[args[i]].First().Value * (int.TryParse(args[i + 1], out var x) ? x : 0) + sum);
+var tabetamono = Enumerable.Range(0, args.Length / 2).Select(i => i * 2 + 1).Aggregate("", (sum, i) => $"{sum}\n{menu[args[i]].First().Key}\t{menu[args[i]].First().Value:d4} 円 {(int.TryParse(args[i + 1], out var x) ? x : 0):d2} 個");
+
+Console.WriteLine($"合計金額: {kingaku} 円, おつり: {(int.TryParse(args[0], out var y) ? y : 0) - kingaku} 円");
+Console.WriteLine($"食べたもの: {tabetamono}");
